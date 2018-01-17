aab9daaf951b028179b086385c1f913b34ac81e3
chaminw <chamin.13@cse.mrt.ac.lk>
2018-01-17 16:25:19 +0530

    Add dao package

--- src/main/java/com/sysco/app/dao/OrderDAO.java
+++ src/main/java/com/sysco/app/dao/OrderDAO.java
@@ -1,4 +1,11 @@
 package com.sysco.app.dao;
 
+import com.sysco.app.model.Order;
+import com.sysco.app.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
 public class OrderDAO {
+
 }
