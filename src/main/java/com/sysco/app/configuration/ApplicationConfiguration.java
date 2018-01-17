7f6f4b04c7e937df077eed1450167b4337a199f2
chaminw <chamin.13@cse.mrt.ac.lk>
2018-01-17 17:00:28 +0530

    Remove redundant classes

--- src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
+++
@@ -1,4 +0,0 @@
-package com.sysco.app.configuration;
-
-public class ApplicationConfiguration {
-}
--- src/main/java/com/sysco/app/configuration/MongoConfiguration.java
+++ src/main/java/com/sysco/app/configuration/MongoConfiguration.java
@@ -1,4 +1,23 @@
 package com.sysco.app.configuration;
 
-public class MongoConfiguration {
+import com.mongodb.MongoClient;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+
+
+@Configuration
+@EnableMongoRepositories(basePackages = "com.sysco.app.repository")
+public class MongoConfiguration extends AbstractMongoConfiguration {
+
+    protected String getDatabaseName() {
+        return "springTest";
+    }
+
+    public MongoClient mongoClient() {
+        return new MongoClient("127.0.0.1",27017);
+    }
+
+
 }
+
