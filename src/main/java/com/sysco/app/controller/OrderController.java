16d3d14f9c4f04c7f6671d398d9ecc6dc479c2b4
Roshan Alwis <roshanalwis@Roshans-MacBook-Pro.local>
2018-01-21 10:29:10 +0530

    Change Log4j dependency

--- src/main/java/com/sysco/app/controller/OrderController.java
+++ src/main/java/com/sysco/app/controller/OrderController.java
@@ -1,19 +1,27 @@
 package com.sysco.app.controller;
 
 import com.sysco.app.repository.OrderRepository;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/order")
 public class OrderController {
 
     @Qualifier("OrderRepository")
     @Autowired
     OrderRepository orderRepository;
 
+    private Logger logger = Logger.getLogger(OrderController.class);
 
 
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public ResponseEntity<String> index() {
+        return new ResponseEntity<String>("Success", HttpStatus.OK);
+    }
 }
