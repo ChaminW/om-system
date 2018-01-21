5d325339e2babe685771e1caa0710a19c5ba38be
Roshan Alwis <roshanalwis@Roshans-MacBook-Pro.local>
2018-01-21 10:41:19 +0530

    Change MainConfiguration to ApplicationConfiguration

--- src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
+++ src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
@@ -7,7 +7,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.sysco.app")
-public class MainConfiguration {
+public class ApplicationConfiguration {
 
 
 }
