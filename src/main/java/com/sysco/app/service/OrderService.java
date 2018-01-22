961a96c2b49a6722f6b5c6d807da2c9bbd919178
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-01-22 08:40:17 +0530

    Implement OrderService interface

--- src/main/java/com/sysco/app/service/OrderService.java
+++ src/main/java/com/sysco/app/service/OrderService.java
@@ -1,4 +1,16 @@
 package com.sysco.app.service;
 
 public interface OrderService {
+
+    void createOrder(Order order);
+
+    List<Order> readOrder(Order order);
+
+    List<Order> readOrder(long id);
+
+    void updateOrder(Order order);
+
+    void deleteOrder(Order order);
+
+    void delteOrder(long id);
 }
