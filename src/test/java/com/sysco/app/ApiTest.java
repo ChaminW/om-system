e0aeb9ce3c3b683392922db1d5b763bf6547cffb
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-01-22 17:22:59 +0530

    mockito and testng defendencies in pom

---
+++ src/test/java/com/sysco/app/ApiTest.java
@@ -0,0 +1,17 @@
+package com.sysco.app;
+
+import com.sysco.app.model.Order;
+import org.junit.Test;
+import org.mockito.Mock;
+
+public class ApiTest {
+
+    @Mock
+    Order order;
+
+    @Test
+    public void testGet() {
+
+
+    }
+}
