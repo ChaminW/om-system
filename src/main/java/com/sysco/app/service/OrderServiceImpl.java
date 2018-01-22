3454b6dbf0f655778dd3d5b12d513c5957847a15
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-01-22 09:18:31 +0530

    Define transactional operations in OrderServiceImpl

--- src/main/java/com/sysco/app/service/OrderServiceImpl.java
+++ src/main/java/com/sysco/app/service/OrderServiceImpl.java
@@ -1,10 +1,18 @@
 package com.sysco.app.service;
 
 import com.sysco.app.model.Order;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
+@Component
 public class OrderServiceImpl implements OrderService {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderServiceImpl.class);
+
+    @Transactional
     @Override
     public void createOrder(Order order) {
 
@@ -20,16 +28,19 @@ public class OrderServiceImpl implements OrderService {
         return null;
     }
 
+    @Transactional
     @Override
     public void updateOrder(Order order) {
 
     }
 
+    @Transactional
     @Override
     public void deleteOrder(Order order) {
 
     }
 
+    @Transactional
     @Override
     public void deleteOrder(long id) {
 
