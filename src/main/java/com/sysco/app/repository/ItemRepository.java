56edb75cc8cbbe33a65c379755d7a24f05b1d0b9
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-01-23 16:41:07 +0530

    Implement query by item name

--- src/main/java/com/sysco/app/repository/ItemRepository.java
+++ src/main/java/com/sysco/app/repository/ItemRepository.java
@@ -2,12 +2,10 @@ package com.sysco.app.repository;
 
 import com.sysco.app.model.Item;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository("itemRepository")
 public interface ItemRepository extends MongoRepository<Item, String> {
-    @Query("{ 'name' : ?0 }")
     List<Item> findItemsByName(String name);
 }
