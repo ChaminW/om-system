c6f0ecfc4def7dae9db002f4a9aff431a25acc9c
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-01-24 15:02:59 +0530

    remove inheritance

--- src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
+++ src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
@@ -4,14 +4,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.sysco.app")
-public class ApplicationConfiguration extends WebMvcConfigurerAdapter {
+public class ApplicationConfiguration {
 
-    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
