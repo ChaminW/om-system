b4215a3b839c5182cb44d072cb8a6e630bf93604
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-01-24 15:27:25 +0530

    inherited to WebMvcAdapter

--- src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
+++ src/main/java/com/sysco/app/configuration/ApplicationConfiguration.java
@@ -4,12 +4,14 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.sysco.app")
-public class ApplicationConfiguration {
+public class ApplicationConfiguration extends WebMvcConfigurerAdapter{
 
+    @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
