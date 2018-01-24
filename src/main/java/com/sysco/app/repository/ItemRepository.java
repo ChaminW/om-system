a979d8e5bda9a7422839839892c4bb3440fc0029
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-01-24 15:20:16 +0530

    fix merge issues

--- src/main/java/com/sysco/app/repository/ItemRepository.java
+++ src/main/java/com/sysco/app/repository/ItemRepository.java
@@ -7,10 +7,5 @@ import java.util.List;
 
 @Repository
 public interface ItemRepository extends MongoRepository<Item, String> {
-<<<<<<< HEAD
     List<Item> findItemsById(String id);
-=======
-    List<Item> findItemsByName(String name);
-
->>>>>>> be3f54e64a47c057437609138322759d63eec9aa
 }
