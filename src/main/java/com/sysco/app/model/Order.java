e971f301b360d20f50dcce84e684980767460818
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-02-08 15:37:28 +0530

    merging

--- src/main/java/com/sysco/app/model/Order.java
+++ src/main/java/com/sysco/app/model/Order.java
@@ -3,7 +3,6 @@ package com.sysco.app.model;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.format.annotation.DateTimeFormat;
-
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import java.util.Date;
