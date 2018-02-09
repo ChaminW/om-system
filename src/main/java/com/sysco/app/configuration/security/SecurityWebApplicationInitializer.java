bc5ce390c3c36bcff8ddc1a16cf908274a9477a5
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-02-09 14:29:13 +0530

    Implementing securtiy features

---
+++ src/main/java/com/sysco/app/configuration/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,6 @@
+//package com.sysco.app.configuration.security;
+//
+//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+//
+//public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+//}
