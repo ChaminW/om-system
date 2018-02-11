422c5fcdd6035d9142a9635604255d35752f4de0
asankai <waiasanka.13@cse.mrt.ac.lk>
2018-02-11 18:52:49 +0530

    remove qualifier

--- src/main/java/com/sysco/app/repository/ItemRepository.java
+++ src/main/java/com/sysco/app/repository/ItemRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository(value = "itemRepository")
+@Repository
 public interface ItemRepository extends MongoRepository<Item, String>, PagingAndSortingRepository<Item, String> {
     Item findItemById(String id);
 }
