6e39231ae058787ccfbb01a61df74712335322f4
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-02-12 10:02:00 +0530

    Security integrations

--- src/main/java/com/sysco/app/configuration/security/SecurityWebApplicationInitializer.java
+++
@@ -1,6 +0,0 @@
-//package com.sysco.app.configuration.security;
-//
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//
-//public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
-//}
