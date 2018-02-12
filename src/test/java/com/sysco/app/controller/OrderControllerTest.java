412d5aabbad174648853292a4e88caff1b971464
Amoda Dissanayake <amodadissanayake@gmail.com>
2018-02-12 09:54:35 +0530

    Tests adjusted to test environment

--- src/test/java/com/sysco/app/controller/OrderControllerTest.java
+++ src/test/java/com/sysco/app/controller/OrderControllerTest.java
@@ -3,15 +3,24 @@ package com.sysco.app.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sysco.app.configuration.ApplicationConfiguration;
 import com.sysco.app.model.Order;
+import com.sysco.app.repository.ItemRepository;
+import com.sysco.app.repository.OrderRepository;
+import com.sysco.app.service.ItemServiceImpl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
 import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Timed;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.support.AnnotationConfigContextLoader;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
@@ -20,20 +29,32 @@ import org.springframework.web.context.WebApplicationContext;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.Date;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 
 @RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles(profiles = {"test"})
 @ContextConfiguration(classes = { ApplicationConfiguration.class })
 @WebAppConfiguration
-public class OrderControllerTest {
+public class OrderControllerTest{
 
     @Autowired
     private WebApplicationContext context;
 
+    @Autowired
+    private ConfigurableEnvironment env;
+
     private MockMvc mockMvc;
+
+    @Mock
+    OrderRepository orderRepository;
+
+//    @InjectMocks
+//    ItemServiceImpl itemService;
 
     @Before
     public void setUp() {
@@ -43,6 +64,12 @@ public class OrderControllerTest {
     /**
      * Integration testing for /orders API endpoint
      */
+
+//    @Test
+//    public void findStudentByName_should_return_order() {
+//        assertEquals("Thijs", orderRepository.existsById(""));
+//    }
+
     @Test
     @Timed(millis=1000)
     public void givenOrders_whenMockMVC_thenVerifyResponseOK() throws Exception {
@@ -89,14 +116,14 @@ public class OrderControllerTest {
 
     @Test
     public void givenOrdersWithPostAndFormData_whenMockMVC_thenResponseCREATED() throws Exception {
-        Order order = new Order("5a5f705d062cb49fbcd43ad7","123kj312c062cb49fbcd43ad8","shipping","pending", Date.from(Instant.now()),Date.from(Instant.now()),Date.from(Instant.now()),"",new ArrayList<String>(){{add("5a5f411f062cb49fbcd43ad6");}});
+        Order order = new Order("5a7febf9638e3868962c17af","123kj312c062cb49fbcd43ad8","shipping","pending", Date.from(Instant.now()),Date.from(Instant.now()),Date.from(Instant.now()),"",new ArrayList<String>(){{add("5a5f411f062cb49fbcd43ad6");}});
         this.mockMvc.perform(post("/orders")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content(new ObjectMapper().writeValueAsString(order)))
                 .andDo(print())
                 .andExpect(status().isCreated())
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
-                .andExpect(jsonPath("$.restaurantId").value("5a5f705d062cb49fbcd43ad7"))
+                .andExpect(jsonPath("$.restaurantId").value("5a7febf9638e3868962c17af"))
                 .andExpect(jsonPath("$.deliveryAddressId").value("123kj312c062cb49fbcd43ad8"))
                 .andExpect(jsonPath("$.deliveryMethod").value("shipping"))
                 .andExpect(jsonPath("$.status").value("pending"));
@@ -154,6 +181,8 @@ public class OrderControllerTest {
                 .andExpect(content().contentType("application/json;charset=UTF-8"))
                 .andExpect(jsonPath("$.message").value("updateOrder.id: Id should be of varchar type"));
     }
+
+
 
     @After
     public void tearDown() {
