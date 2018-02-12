4fcdc763dcf91984c16f39527abb63bdabb4016e
Amoda Dissanayake <amodadissanayake@gmail.com>
2018-02-12 17:15:58 +0530

    Restaurant Id validated

--- src/main/java/com/sysco/app/annotation/CheckRestaurantId.java
+++ src/main/java/com/sysco/app/annotation/CheckRestaurantId.java
@@ -5,10 +5,10 @@ import javax.validation.Payload;
 import java.lang.annotation.*;
 
 @Documented
-@Constraint(validatedBy = RestaurantExistValidator.class)
+@Constraint(validatedBy = RestaurantIdValidator.class)
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
-public @interface CheckRestaurant{
+public @interface CheckRestaurantId {
     String message() default "A restuarant should exit to add an order";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
