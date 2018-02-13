8a0748fa1ae1c35c9da1a89c1d89f7c353cade13
chaminw <chamin.13@cse.mrt.ac.lk>
2018-02-13 12:16:20 +0530

    Code formatting

--- src/main/java/com/sysco/app/annotation/ValidUntilDateValidator.java
+++ src/main/java/com/sysco/app/annotation/ValidUntilDateValidator.java
@@ -1,9 +1,5 @@
 package com.sysco.app.annotation;
 
-
-import com.sysco.app.repository.RestaurantRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
