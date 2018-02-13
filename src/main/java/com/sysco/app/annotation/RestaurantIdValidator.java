3664d1e6d4ef511f4946744020608ccfb58c0f27
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-02-13 07:41:28 +0530

    Basic cleaning

--- src/main/java/com/sysco/app/annotation/RestaurantIdValidator.java
+++ src/main/java/com/sysco/app/annotation/RestaurantIdValidator.java
@@ -6,7 +6,6 @@ import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
-
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
