4d5347851e992e482aa82d52de19bd7ef1c5f7d3
chaminw <chamin.13@cse.mrt.ac.lk>
2018-02-13 12:16:31 +0530

    Code formatting

--- src/main/java/com/sysco/app/exception/SystemException.java
+++ src/main/java/com/sysco/app/exception/SystemException.java
@@ -3,11 +3,11 @@ package com.sysco.app.exception;
 import java.time.LocalDateTime;
 
 public class SystemException extends RuntimeException {
-    final private ErrorCode errorCode;
-    final private Class rootClass;
-    final private String timestamp;
+    private final ErrorCode errorCode;
+    private final Class rootClass;
+    private final String timestamp;
 
-    public SystemException(String message, ErrorCode errorCode, Class rootClass) {
+    SystemException(String message, ErrorCode errorCode, Class rootClass) {
         super(message);
         this.errorCode = errorCode;
         this.rootClass = rootClass;
--- src/main/java/com/sysco/app/service/OrderServiceImpl.java
+++ src/main/java/com/sysco/app/service/OrderServiceImpl.java
@@ -14,7 +14,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.validation.Errors;
 import java.util.List;
 
 @Component("orderService")
