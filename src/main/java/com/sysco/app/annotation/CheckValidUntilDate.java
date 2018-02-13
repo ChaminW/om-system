f1dcb48ed17de0c2f5b03802559fb0ce5cb4f3b2
Amoda Dissanayake <amodadissanayake@gmail.com>
2018-02-13 11:20:14 +0530

    Nullable allowed for valid until

--- src/main/java/com/sysco/app/annotation/CheckValidUntilDate.java
+++ src/main/java/com/sysco/app/annotation/CheckValidUntilDate.java
@@ -9,6 +9,7 @@ import java.lang.annotation.*;
 @Target( { ElementType.METHOD, ElementType.FIELD })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface CheckValidUntilDate {
+    String message() default "valid until date should be future";
     Class<?>[] groups() default {};
     Class<? extends Payload>[] payload() default {};
 }
