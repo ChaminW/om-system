36ee34bc7fdb145001ae3f92da86b4d71b6e3233
roshanmadhushanka <alwisroshan.13@cse.mrt.ac.lk>
2018-02-13 07:04:30 +0530

    Cleaning RootController

--- src/main/java/com/sysco/app/controller/RootController.java
+++ src/main/java/com/sysco/app/controller/RootController.java
@@ -3,8 +3,6 @@ package com.sysco.app.controller;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,23 +13,13 @@ import springfox.documentation.annotations.ApiIgnore;
 @ApiIgnore
 public class RootController {
 
-    private final
-    MessageSource messageSource;
-
-
     private Logger logger = LoggerFactory.getLogger(OrderController.class);
-
-    @Autowired
-    public RootController(MessageSource messageSource) {
-        this.messageSource = messageSource;
-    }
 
     @ApiOperation(value = "view API documentation", produces = "test/HTML")
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public RedirectView rootService() {
 
         logger.info("Service initiated");
-
         return new RedirectView("/swagger-ui.html");
     }
 
